9473e3f20ed9c740a6268f7dd362a9e156e30d91
Vitória <cristina.almeida@escolar.ifrn.edu.br>
2026-05-30 11:54:52 -0300

    Post

--- main.py
+++ main.py
@@ -23,3 +23,29 @@ async def get_db_connection():
         database="UTI dos Motores",
         host="localhost"
     )
+
+@app.get("/status")
+async def status():
+    return {"mensagem": "API funcionando"}
+
+@app.post("/ordens")
+async def criar_ordem(ordem: OrdemCriada):
+
+    conn = await get_db_connection()
+
+    result = await conn.execute(
+        """
+        INSERT INTO ordens_servico (nome_cliente, motor, problema)
+        VALUES ($1, $2, $3)
+        """,
+        ordem.nome_cliente,
+        ordem.motor,
+        ordem.problema
+    )
+
+    await conn.close()
+
+    if result == "INSERT 0 1":
+        return {"message": "Ordem de serviço criada com sucesso!"}
+    else:
+        return {"message": "Ordem de serviço não foi criada."}
